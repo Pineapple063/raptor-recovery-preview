8dfcd06d5f0f3485dacf96b9fda886744ea57732
Daniel M <dan1992iel@gmail.com>
2025-03-20 13:28:43 +0000

    changed the App.jsx file

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import { useState, useEffect } from 'react'
-import { BrowserRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
+import { HashRouter as Router, Routes, Route, useLocation, Navigate } from "react-router-dom";
 import Home from "./pages/Home";
 import Services from './pages/Services';
 import FAQs from './pages/FAQs';
