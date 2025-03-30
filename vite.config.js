a4418a4f89019e89862041d43e0e50fe735de779
Daniel M <dan1992iel@gmail.com>
2025-03-30 14:37:58 +0100

    test

--- vite.config.js
+++ vite.config.js
@@ -4,7 +4,7 @@ import react from '@vitejs/plugin-react'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/raptor-recovery-preview",
+  base: "/",
   server: {
     host: true,
     proxy: {
